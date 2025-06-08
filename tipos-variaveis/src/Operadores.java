d2f288e920c79422bcc3927812431f099d290047
Thiago Rodrigues <thiagorodriguessa7@gmail.com>
2025-06-08 11:45:55 -0300

    Nova classe mostando operadores na pratica

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,8 +1,31 @@
 public class Operadores {
 
     public static void main(String[] args) {
-        String nomeCompleto = "Linguagem" + "JAVA";
+        String nome1 = "Gleison";
+        String nome2 = new String("Gleison");
 
-        System.out.println(nomeCompleto);
+        System.out.println(nome1.equals(nome2));
+
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        if (numero1 < numero2) {
+            System.out.println("A nossas condição é verdadeira");
+        }
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("N1 é igual a n2? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("N1 é igual a n2? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("N1 é igual a n2? " + simNao);
+
     }
 }
