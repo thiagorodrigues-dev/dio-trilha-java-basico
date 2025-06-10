4c918312306e3a5ed11a9acbcca6fed53653c86a
Thiago Rodrigues <thiagorodriguessa7@gmail.com>
2025-06-10 14:45:44 -0300

    Introdução ao for e como podemos usar

---
+++ estrutura-repeticao/src/For/ExemploFor.java
@@ -0,0 +1,9 @@
+package For;
+
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++) {
+            System.out.println("Contando carneirinhos: " + carneirinhos);
+        }
+    }
+}
