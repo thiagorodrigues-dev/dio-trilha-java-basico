d35c755e50a1d0661a774bac20449d7e54449831
Thiago Rodrigues <thiagorodriguessa7@gmail.com>
2025-06-10 14:03:11 -0300

    Colocando em pratica condiconais encadeadas e ternarias

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,11 +1,24 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 10;
 
         if (nota >= 7) {
             System.out.println("Aprovado");
+        } else if (nota >= 5 && nota < 7) {
+            System.out.println("Recuperação");
         } else {
             System.out.println("Reprovado");
         }
+
+        System.out.println("---operacao ternaria---");
+
+        //operacao ternaria
+        int nota2 = 7;
+        String resultado = nota2 >= 7 ? "Aprovado" : "Reprovado";
+
+        System.out.println(resultado);
+
+        System.out.println("----mais um exemplo de operacao ternaria----");
+        String resultado2 = nota2 >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperacao" : "Reprovado";
     }
 }
